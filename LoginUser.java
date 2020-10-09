89d5821eb1d60e75558033cc3470b04177f69f8d
Nageswara Rao P <info@wiculty.com>
2020-10-09 21:52:31 +0530

    login code

--- LoginUser.java
+++ LoginUser.java
@@ -1,11 +1,12 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-
+public class Login
+{
+	public static void main(String args[])
+	{
+		int i;
+		
+		for(i=0;i<=10;i++)
+		{
+			System.out.println("Number: " + i);
+		}
+	}
+}
